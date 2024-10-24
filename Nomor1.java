0f009ff23fd204fe8bf4eecc9491c4767f1252ea
inayafaridah <inayaafaridah@gmail.com>
2024-10-24 10:36:20 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,7 +1,27 @@
-
+import java.util.Scanner;
 
 public class Nomor1{
     public static void main(String[] args){
-      
+        Scanner input = new Scanner (System.in);
+        double penjualan= 0;
+        double komisi;
+        double komisiPertama;
+        double totalKomisi;
+
+        System.out.print("Masukkan total penjualan: ");
+        double totalPenjualan = input.nextDouble();
+    
+        if (penjualan == 500000) {
+            totalKomisi = totalPenjualan * 0.10;
+        } else if (penjualan >= 500000) {
+            komisiPertama = 500000 * 0.10;
+            komisi = (totalPenjualan - 500000) * 0.15;
+            totalKomisi = komisiPertama + komisi;
+        } else {
+            totalKomisi = 0.0;
+        }
+        
+        System.out.println("Komisi yang didapatkan: "+ totalKomisi);
+        input.close();
     }
 }
